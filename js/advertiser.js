fb17068b62ad170b82a8839a2f7327b80e170f96
SebOuellette <seb_ouellette@honeybeeks.net>
2023-10-21 12:18:17 -0400

    Initial commit
    Contains initial bare class for cart, and a file for advertiser code.
    
    Js files are not included by the html

---
+++ js/advertiser.js
@@ -0,0 +1,6 @@
+// This file will control advertisements within this page
+(()=>{ // This symbol puts all variables within a scope, preventing users from tampering with them
+
+
+    
+});
---
+++ js/cart.js
@@ -0,0 +1,11 @@
+// This file contains the cart object and relative tasks
+(()=>{
+
+    class Cart {
+        constructor() {
+            // this.x = 1
+            // this.y = 2
+        }
+    }
+
+});
