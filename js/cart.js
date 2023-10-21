6699a066fc5234a1769f921f24bc985695658ef5
SebOuellette <seb_ouellette@honeybeeks.net>
2023-10-21 12:42:41 -0400

    Find profile URL from profile ID
    Created a profile stub

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,21 @@
 // This file contains the cart object and relative tasks
-(()=>{
+//(()=>{
 
     class Cart {
         constructor() {
             // this.x = 1
             // this.y = 2
+
+
+        }
+
+
+        getProfileLink(profile) {
+            let domain = document.location.hostname;
+            let profileID = profile.getProfileID();
+
+            return `https://${domain}/users/${profileID}`;
         }
     }
 
-});
+//});
