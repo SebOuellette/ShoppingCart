33b9d1945eeb4a39f18f4bd55302bca8647afb0c
SebOuellette <seb_ouellette@honeybeeks.net>
2023-10-25 19:03:49 -0400

    Working webserver template

---
+++ Shared/main.cpp
@@ -0,0 +1,45 @@
+#define CROW_MAIN
+
+#include "crow_all.h"
+#include <regex>
+#include <iostream>
+using namespace std;
+using namespace crow;
+
+std::string loadFile(response& res, std::string _folder, std::string _name);
+
+int main()
+{
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/") // Products Page
+		([](const request& req, response& res){
+			res.set_header("Content-Type", "text/html");
+			
+			res.write(loadFile(res, "", "index.html"));
+			
+			res.end();
+		});
+
+	app.port(23500).multithreaded().run();
+
+	return 1;
+}
+
+string loadFile(response& res, std::string _folder, std::string _name) {
+	std::string path = "../public/" + _folder + _name;
+
+	ifstream file(path, ifstream::in);
+
+	if (file) {
+		ostringstream contents;
+		contents << file.rdbuf();
+		file.close();
+		return contents.str();
+	}
+	else {
+		res.set_header("Content-Type", "text/plain");
+		res.code = 404;
+		return path + "Not Found";
+	}
+}
---
+++ Shared/public/js/cart.js
@@ -0,0 +1,17 @@
+// This file contains the cart object and relative tasks
+//(()=>{
+    class Cart {
+        constructor() {
+            // this.x = 1
+            // this.y = 2
+        }
+
+
+        getProfileLink(profileID) {
+            let domain = window.location.origin;
+
+            return `${origin}/users/${profileID}`;
+        }
+    }
+
+//});
