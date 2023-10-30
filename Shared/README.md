cd577321cf10820564bfe4283e31f1ac474100d3
SebOuellette <seb_ouellette@honeybeeks.net>
2023-10-30 15:29:51 -0400

    Ability to link to profile page within profile module
    Satisfies requirement 360
    
    360 is not done, however, becuase we still need to wait for the profile module to decide on an endpoint. For now, 360 will be moved into HOLD status.

---
+++ Shared/README.md
@@ -0,0 +1,7 @@
+# Shopping Cart Module
+
+This is the shopping cart module for CSCN73030 Lab 2. 
+
+## Documentation
+
+### 
--- Shared/main.cpp
+++ Shared/main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace crow;
 
+#define PROFILE "http://localhost:8081"
+
 std::string loadFile(response& res, std::string _folder, std::string _name);
 
 int main()
@@ -18,6 +20,32 @@ int main()
 			
 			res.write(loadFile(res, "", "index.html"));
 			
+			res.end();
+		});
+
+
+	CROW_ROUTE(app, "/profile/<int>")
+	.methods(HTTPMethod::GET)
+		([](const request& req, response& res, int profileID){
+			std::string pidString = std::to_string(profileID);
+
+			// Temporary Redirect
+			res.code = 307;
+
+			// Set the redirection URL
+			std::stringstream profileURL;
+
+			// THE PROFILE MODULE DOES NOT HAVE A DECIDED URL FOR PROFILES AT THE TIME OF WRITING
+			// Once the module does have a decided url endpoint, the below line MUST be updated to point to the correct url
+
+			profileURL << PROFILE << "/WE_DONT_KNOW_YET/" << pidString << "/";
+			res.set_header("Location", profileURL.str());
+			
+			// If the browser or client does not redirect, they will see a message showing where we are trying to redirect to
+			std::stringstream message;
+			message << "Redirecting to profile page for user: '" << pidString << "' at " << profileURL.str();
+			res.write(message.str());
+
 			res.end();
 		});
 
